a05b17aabfbd9b13b10dee56bd250037029b1e89
luckyeven <ssong094@uottawa.ca>
2021-11-15 09:19:19 +0000

    implement capture_pi.py

--- data_collection_pi/capture_pi.py
+++ data_collection_pi/capture_pi.py
@@ -5,7 +5,7 @@ Author: Shifeng Song
 Date: Nev 13, 2021
 
 """
-
+import time
 import cv2
 from picamera import PiCamera
 from picamera.array import PiRGBArray
@@ -15,7 +15,7 @@ res_width = 96                     # width of camera
 res_height = 96                    # height of camera
 rotation = 0                       # Camera rotation (0,90,180,or 270)
 draw_fps = False                   # Display fps on screen
-save_path = "../images/num01"      # Save images to directory
+save_path = "../images/num01/"      # Save images to directory
 file_num = 0                       # Starting point for filename
 file_suffix = ".png"               # image Extension
 precountdown = 2                   # Seconds before staring countdown
@@ -29,7 +29,7 @@ fps = 0
 
 def file_exists(filepath):
     """
-    Return true if file exists, false otherwise
+    Returns true if file exists, false otherwise
     """
     try:
         f = open(filepath, 'r')
@@ -39,83 +39,100 @@ def file_exists(filepath):
         exists = False
     return exists
 
+
 def get_filepath():
     """
-    Returns the next avaiable full path to image file
+    Returns the next available full path to image file
     """
 
     global file_num
 
-    # loop through possible file numbers to see if that file already exists
+    # Loop through possible file numbers to see if that file already exists
     filepath = save_path + str(file_num) + file_suffix
     while file_exists(filepath):
         file_num += 1
         filepath = save_path + str(file_num) + file_suffix
 
     return filepath
-    ######################################################
-    # Main
 
-    # Figure out the name of the output image filename
-    filepath = get_filepath()
+################################################################################
+# Main
 
-    #Start the camera
-    with PiCamera() as camera:
+# Figure out the name of the output image filename
+filepath = get_filepath()
 
-        # Configure camera settings
-        camera.resolution = (res_width, res_height)
-        camera.rotation = rotation
+# Start the camera
+with PiCamera() as camera:
 
-        # Container for out frames
-        raw_capture = PiRGBArray(camera, size = (res_width, res_height))
-
-        # Initial countdown timestamp
-        countdown_timestamp = cv2.getTickcount()
-
-        # Continuosusly capture frames
-        for frame in camera.capture_continuous(raw_capture,format='bgr', use_video_port = True):
-            # Get timestamp for calculationg actual framerate
-            timestamp = cv2.getTickCount()
-
-            # GetNumpy array that represents the image
-            img = frame.array
-
-            # Each second, decrement countdown
-            if(timestamp - countdown_timestamp) / cv2.getTickFrequency() > 1.0:
-                countdown_timestamp = cv2.getTickCount()
-                countdown -= 1
-
-                # When countdown reaches 0 ,break out of loop to save image
-                if countdown <= 0:
-                    countdown = 0
-                    break
+    # Configure camera settings
+    camera.resolution = (res_width, res_height)
+    camera.rotation = rotation
     
-            # Fraw countdown on screen
-            cv2.putText(img,
-                        str(countdown),
-                        (int(round(res_width / 2) - 5),
-                            int(round(res_height / 2))),
+    # Container for our frames
+    raw_capture = PiRGBArray(camera, size=(res_width, res_height))
+    
+    # Initial countdown timestamp
+    countdown_timestamp = cv2.getTickCount()
+
+    # Continuously capture frames (this is our while loop)
+    for frame in camera.capture_continuous(raw_capture, 
+                                            format='bgr', 
+                                            use_video_port=True):
+                                            
+        # Get timestamp for calculating actual framerate
+        timestamp = cv2.getTickCount()
+        
+        # Get Numpy array that represents the image
+        img = frame.array
+       
+        # Each second, decrement countdown
+        if (timestamp - countdown_timestamp) / cv2.getTickFrequency() > 1.0:
+            countdown_timestamp = cv2.getTickCount()
+            countdown -= 1
+            
+            # When countdown reaches 0, break out of loop to save image
+            if countdown <= 0:
+                countdown = 0
+                break
+                
+        
+        # Draw countdown on screen
+        cv2.putText(img,
+                    str(countdown),
+                    (int(round(res_width / 2) - 5),
+                        int(round(res_height / 2))),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    1,
+                    (255, 255, 255))
+                    
+        # Draw framerate on frame
+        if draw_fps:
+            cv2.putText(img, 
+                        "FPS: " + str(round(fps, 2)), 
+                        (0, 12),
                         cv2.FONT_HERSHEY_PLAIN,
                         1,
                         (255, 255, 255))
-            
-            # sHOW THE FRAME
-            CV2.imshow("Frame", img)
-
-            # Clear the stream to prepare for next fram
-            raw_capture.truncate(0)
-
-            # Calculate framrate
-            frame_time = (cv2.getTickCount() - timestampe) / cv2.getTickFrequency()
-            fps = 1 / frame_time
-
-            # Press 'q' to quit
-            if cv2.waitKey(1) == ord('q'):
-                break
         
-        # Capture image
-        camera.capture(filepath)
-        print("Image saved to: ", filepath)
+        # Show the frame
+        cv2.imshow("Frame", img)
+        
+        # Clear the stream to prepare for next frame
+        raw_capture.truncate(0)
+        
+        # Calculate framrate
+        frame_time = (cv2.getTickCount() - timestamp) / cv2.getTickFrequency()
+        fps = 1 / frame_time
+        
+        # Press 'q' to quit
+        if cv2.waitKey(1) == ord('q'):
+            break
+    
+    # Capture image
+
+    camera.capture(filepath)
+    print("Image saved to:", filepath)
+ 
 
 # Clean up
 cv2.destroyAllWindows()
