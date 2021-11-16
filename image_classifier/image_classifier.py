a1b00a2cd7d432754b59173c6ad3da09f4810318
luckyeven <ssong094@uottawa.ca>
2021-11-16 02:07:52 +0000

    update

--- image_classifier/image_classifier.py
+++ image_classifier/image_classifier.py
@@ -77,3 +77,280 @@ labels = sorted(labels)
 # Print out labels and number of samples
 print(labels)
 print("Number of samples:", num_samples)
+
+### Convert labels to numbers
+
+# Show the labels before the conversion
+print("Before:", y_all)
+
+# Convert each label to its index in the labels
+y_out = []
+for i, label in enumerate(y_all):
+  y_out.append(labels.index(label))
+y_all = y_out
+
+# Show the labels after the conversion
+print("After:", y_all)
+
+### Shuffle samples and labels together, divide into test, validation, and training sets
+
+# Shuffle samples and associated labels together
+X_y = list(zip(X_all, y_all))
+random.shuffle(X_y)
+X_all, y_all = zip(*X_y)
+
+# Calculate number of validation and test samples to put aside (round down)
+num_samples_test = int(TEST_RATIO * num_samples)
+num_samples_val = int(VAL_RATIO * num_samples)
+
+# The first `num_samples_test` samples of the shuffled list becomes the test set
+X_test = X_all[:num_samples_test]
+y_test = y_all[:num_samples_test]
+
+# The next `num_samples_val` samples of the shuffled list becomes the validation set
+X_val = X_all[num_samples_test:(num_samples_test + num_samples_val)]
+y_val = y_all[num_samples_test:(num_samples_test + num_samples_val)]
+
+# The remaining samples become the training set
+X_train = X_all[(num_samples_test + num_samples_val):]
+y_train = y_all[(num_samples_test + num_samples_val):]
+
+# Remember the number of samples in the test set
+num_samples_train = len(X_train)
+
+# Print out the number of test, validation, and training samples
+print("Number of test samples:", num_samples_test)
+print("Number of validation samples:", num_samples_val)
+print("Number of training samples:", num_samples_train)
+
+### View one of the training samples
+
+# Chose which sample you want to view
+idx = 0
+
+# Print out label (numbe and string) and part of the array
+print("Label: " + str(y_train[idx]) + " (" + labels[y_train[idx]] + ")")
+print(X_train[idx])
+
+# Display image from array
+plt.imshow(X_train[idx], cmap='gray', vmin=0, vmax=255)
+img.show()
+
+
+### Function to resize list of images
+def resize_images(images, width, height, anti_aliasing=True):
+  """
+  Prove a list of Numpy arrays (in images parameter) to have them all resized to desired height and
+  width. Returns the list of newly resized image arrays.
+
+  NOTE: skimage resize returns *normalized* image arrays (values between 0..1)
+  """
+  X_out = []
+  for i, img in enumerate(images):
+    X_out.append(resize(img, (height, width), anti_aliasing=anti_aliasing))
+  return X_out
+
+### Scale/crop images
+
+# Resize (scale) all images in the training set
+X_train = resize_images(X_train, TARGET_WIDTH, TARGET_HEIGHT)
+
+# Resize (scale) all images in the validation set
+X_val = resize_images(X_val, TARGET_WIDTH, TARGET_HEIGHT)
+
+# Resize (scale) all images in the test set
+X_test = resize_images(X_test, TARGET_WIDTH, TARGET_HEIGHT)
+
+### View training sample again (after they all have been scaled)
+
+# Chose which sample you want to view
+idx = 0
+
+# Print out label (numbe and string) and part of the array
+print("Label: " + str(y_train[idx]) + " (" + labels[y_train[idx]] + ")")
+print("First row:", X_train[idx][:1,:])
+
+# Display image from array (note that images have been normalized)
+plt.imshow(X_train[idx], cmap='gray', vmin=0, vmax=1)
+
+
+# Training set
+X_train = np.asarray(X_train)
+y_train = np.asarray(y_train)
+
+# Validation set
+X_val = np.asarray(X_val)
+y_val = np.asarray(y_val)
+
+# Test set
+X_test = np.asarray(X_test)
+y_test = np.asarray(y_test)
+
+# Print out the new Numpy array shapes (always a good idea to check the shapes!)
+print("Training X:", X_train.shape)
+print("Training y:", y_train.shape)
+print("Validation X:", X_val.shape)
+print("Validation y:", y_val.shape)
+print("Test X:", X_test.shape)
+print("Test y:", y_test.shape)
+
+
+### Flatten each image to a 1D vector (DNN requires 1D input)
+
+# Compute length of 1D array that we will flatten each image to
+len_vector = TARGET_WIDTH * TARGET_WIDTH
+
+# Flatten matricies to vectors
+X_train = X_train.reshape(num_samples_train, len_vector)
+X_val = X_val.reshape(num_samples_val, len_vector)
+X_test = X_test.reshape(num_samples_test, len_vector)
+
+# Determine the input shape for our Keras model (must be tuple)
+input_shape = (X_train.shape[1],)
+
+# Print out shapes
+print("X train:", X_train.shape)
+print("y train:", y_train.shape)
+print("X val:", X_val.shape)
+print("y val:", y_val.shape)
+print("X test:", X_test.shape)
+print("y test:", y_test.shape)
+print("Input tensor shape:", input_shape)
+
+### Convert labels (integers) to one-hot encoding
+
+# Get number of classes
+num_classes = len(labels)
+
+# Use Keras's np_utils to create one-hot encoding (note the capital 'Y' - 2D array)
+Y_train = np_utils.to_categorical(y_train, num_classes)
+Y_val = np_utils.to_categorical(y_val, num_classes)
+Y_test = np_utils.to_categorical(y_test, num_classes)
+
+# Print out shapes (capital 'Y' is our one-hot matrix!)
+print("Y train:", Y_train.shape)
+print("Y val:", Y_val.shape)
+print("Y test:", Y_test.shape)
+
+# Print out a few examples from training set
+for i in range(10):
+  print("Label: " + str(y_train[i]) + " | One-hot:", Y_train[i])
+
+
+  ### Construct Keras model
+
+# A sequential model is a linear stack of layers
+model = Sequential()
+
+# First layer: fully-connected with relu activation
+model.add(Dense(64, input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+# Second layer: fully-connected with relu activation
+model.add(Dense(64))   # Input shape is determined automatically from previous layer
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+# Third layer: 10 nodes, one for each class, and softmax activation
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+# Configure the model's training settings
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+# Print out model summary
+print(model.summary())
+
+
+### Train the model
+history = model.fit(X_train, 
+                    Y_train, 
+                    batch_size=32, 
+                    epochs=200, 
+                    verbose=1,
+                    validation_data=(X_val, Y_val))
+
+### Plot training and validation accuracy and loss over time
+
+# Extract accuracy and loss values (in list form) from the history
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Create a list of epoch numbers
+epochs = range(1, len(acc) + 1)
+
+# Plot training and validation loss values over time
+plt.figure()
+plt.plot(epochs, loss, color='blue', marker='.', label='Training loss')
+plt.plot(epochs, val_loss, color='orange', marker='.', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+# Plot training and validation accuracies over time
+plt.figure()
+plt.plot(epochs, acc, color='blue', marker='.', label='Training acc')
+plt.plot(epochs, val_acc, color='orange', marker='.', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.show()
+
+### Try predicting label with one validation sample (inference)
+
+# Change this to try a different sample from the test set
+idx = 0
+
+# Make sample 2D array instead of 1D vector (this is what the Keras model expects as input)
+x = np.expand_dims(X_val[idx], 0)
+
+# Make prediction using trained model
+y_pred = model.predict(x)
+
+# Find index of highest score in output
+predicted_label = np.argmax(y_pred)
+actual_label = np.argmax(Y_val[idx])
+
+# Display model output, predicted label, actual label
+print("Model output:", y_pred)
+print("Predicted label:", predicted_label, "-", labels[predicted_label])
+print("Actual label:", actual_label, "-", labels[actual_label])
+
+
+### Create confusion matrix from validation set
+
+# Find predictions from all validation samples
+Y_pred = model.predict(X_val)
+print("Validation output shape:", Y_pred.shape)
+
+# Convert actual and predicted validation one-hot encoding to numerical labels
+y_val = np.argmax(Y_val, axis=1)
+y_pred = np.argmax(Y_pred, axis=1)
+
+# Print some values from actual and predicted validation sets (first 50 samples)
+print("Actual validation labels:\t", y_val[:50])
+print("Predicted validation labels:\t", y_pred[:50])
+
+# Compute confusion matrix (note: we need to transpose SKLearn matrix to make it match Edge Impulse)
+cm = confusion_matrix(y_val, y_pred)
+cm = np.transpose(cm)
+
+# Print confusion matrix
+print()
+print(" ---> Predicted labels")
+print("|")
+print("v Actual labels")
+print("\t\t\t" + ' '.join("{!s:6}".format('(' + str(i) + ')') for i in range(num_classes)))
+for row in range(num_classes):
+  print("{:>12} ({}):  [{}]".format(labels[row], row, ' '.join("{:6}".format(i) for i in cm[row])))
+
+### Evaluate model on validation set
+score = model.evaluate(X_val, Y_val)
+print("Validation loss:", score[0])
+print("Validation accuracy:", score[1])
+
+### Evaluate model on entire test set
+score = model.evaluate(X_test, Y_test)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
